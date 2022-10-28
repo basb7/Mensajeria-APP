2942256994cea6d8f661063390165182d56dd35b
Brian <brianalbertosuarez@gmail.com>
2022-10-27 22:21:22 -0500

    Conexion a bases de datos, mensajes flash, servicios de recuperacion de usuario

--- app.py
+++ app.py
@@ -1,15 +1,53 @@
-from flask import Flask, render_template, request
+from email import message
+from flask import Flask, flash, render_template, request, session, url_for, redirect
 from markupsafe import escape
+from datetime import datetime
+
+import controler
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key {}'.format(datetime.now)
 
 @app.route('/')
 def index():
     return render_template('login.html')
 
-@app.route('/login')
+@app.route('/dashboard/<user>')
+def dashboard(user):
+    return render_template('dashboard.html', user=(user))
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        user = request.form['user']
+        print(user)
+        password = request.form['password']
+        print(password)
+
+    # Se valida si el usuario existe o esta registrado, inicio de sesión
+        is_user = controler.validate_user(user)
+        print(is_user)
+        if is_user:
+            if is_user[0]['USER'] == user or is_user[0]['EMAIL'] == user and is_user[0]['PASSWORD'] == str(password):
+                session['username'] = user
+                print('Usuario logueado')
+                return redirect(url_for('dashboard', user = is_user[0]['USER']))
+            return redirect(url_for('login'))
+        else:
+            message = 'Usuario no existe, intentelo de nuevo'
+            flash(message, 'danger')
+            print('Usuario no existe')
+            return redirect(url_for('login'))
+
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+        message = 'Sesión finalizada exitosamente!'
+        flash(message, 'success')
+    return redirect(url_for('login'))
 
 @app.route("/<name>")
 def hello(name):
@@ -19,19 +57,98 @@ def hello(name):
 def register():
     if request.method == 'POST':
         email = request.form['email']
+        name = request.form['name']
         username = request.form['username']
         password = request.form['password']
         password2 = request.form['password2']
 
+    # Valida los campos del formulario que no esten vacios
+        if not email or not name or not username or not password or not password2:
+            message = 'Los campos no pueden estar vacios, intente de nuevo!'
+            flash(message, 'danger')
+            return render_template('register.html')
+
+    # Consulta la base de datos en busca de usuario o correos registrados en la base de datos
+        if controler.validate_user(email) == False or controler.validate_user(username) == False:
+            message = 'El correo o usuario ya estan registrados en nuestra base, intenta iniciar sesíon!'
+            flash(message, 'warning')
+            return render_template('register.html')
+
+    # Valida que los campo password sean iguales
         if password != password2:
             message = 'Contraseñas no coinciden'
             print(message)
+            flash(message, 'danger')
             return render_template('register.html')
 
-
+    # Registro de usuario en la base de datos
+        if controler.add_user(email, name, username, password) == True:
+            print('Registro exitoso')
+            message = 'Registro exitoso, inicie sesión'
+            flash(message, 'success')
+            return render_template('login.html')
+        else:
+            message = 'Error al registrar usuario'
+            flash(message, 'danger')       
+            print('Error al registrar usuario')
 
     return render_template('register.html')
 
-@app.route('/recover')
+@app.route('/recover', methods=['GET', 'POST'])
 def recover():
+    if request.method == 'POST':
+        username = request.form['username']
+
+        #Validar que los campos no esten vacios
+        if not username:
+            message = 'Los campos no pueden estar vacios, intente nuevamente!'
+            flash(message, 'danger')
+            return render_template('recover.html')
+
+        #Validar si el usuario existe y enviar correo de recuperacion
+        if controler.validate_user(username) == False:
+            message = 'El usuario no existe, intente registrarse o intentelo de nuevo!'
+            flash(message, 'danger')
+            return render_template('recover.html')
+        
+        is_user = controler.validate_user(username)
+        email_user = is_user[0]['EMAIL']
+
+        def encode_email(email_user):
+            length = 0
+            count = 0
+            arroba = False
+            encode_email = ''
+
+            for i in email_user:
+                if i == '@':
+                    break
+                length += 1
+
+            size = (int(length) / 1.5)
+            
+            for letra in email_user:
+                if letra == '@':
+                    arroba = True
+                
+                if count > size and arroba == False:
+                    letra = '*'
+                    encode_email += letra
+
+                count += 1
+                encode_email += letra
+            
+            return encode_email
+        
+        message = 'Se ha enviado correo de recuperacion a la siguiente dirección {}'.format(encode_email(email_user))
+        flash(message, 'success')
+        return render_template('recover.html')
+
     return render_template('recover.html')
+
+#Proteccion de rutas
+""" @app.before_request
+def load_user():
+    if "username" not in session and (request.endpoint not in ['index', 'login', 'register', 'recover']):
+        print('Debe iniciar sesion')
+        return redirect(url_for('login')) """
--- controler.py
+++ controler.py
@@ -1,12 +1,24 @@
 from re import S
 import sqlite3
+from sqlite3 import Error
+from flask import g
 import random
 
 DB_NAME = 'Database.db'
 
 def connect_db():
-    conn = sqlite3.connect(DB_NAME)
-    return conn
+    try:
+        if 'db' not in g:
+            print('conexion a la base de datos')
+            g.db = sqlite3.connect(DB_NAME)
+        return g.db
+    except Error:
+        print(Error)
+
+def close_db():
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 def add_user(email, name, user, password ):
     code_validation = random.randint(100000, 999999)
@@ -15,8 +27,36 @@ def add_user(email, name, user, password ):
         db = connect_db()
         cursor = db.cursor()
         sql = 'INSERT INTO USERS(NAME, USER, EMAIL, PASSWORD, CODE_VALIDATION, ACCOUNT_VERIFICATE, ROLE) VALUES(?,?,?,?,?,?,?)'
-        cursor.execute(sql, name, user, email, password, code_validation, False, 'user')
+        cursor.execute(sql, [name, user, email, password, code_validation, False, 'user'])
         db.commit()
         return True
     except:
+        return False
+
+def validate_user(username):
+    try:
+        db = connect_db()
+        cursor = db.cursor()
+        sql = 'SELECT * FROM USERS WHERE USER = ? OR EMAIL = ?'
+        cursor.execute(sql, [username, username])
+        result = cursor.fetchone()
+        if result == None:
+            return False
+        else:
+            print(result)
+            usuario = [
+                    {
+                    'id': result[0],
+                    'NAME': result[1],
+                    'USER': result[2],
+                    'EMAIL': result[3],
+                    'PASSWORD': result[4],
+                    'CODE_VALIDATION': result[5],
+                    'ACCOUNT_VERIFICATION': result[6],
+                    'ROLE': result[7]
+                    }
+                ]
+            return usuario
+    except Error:
+        print(Error)
         return False
